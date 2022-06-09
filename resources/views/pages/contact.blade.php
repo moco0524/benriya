a6fc6651d83cd2f08aee7c0a8e4453609f397158
moco0524 <ogawa340910@gmail.com>
2022-06-10 07:31:18 +0900

    20220610-01 レイアウト修正

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -16,59 +16,76 @@
 	@csrf
 		<ul style="width:600px">
 			<li>
-				<span class="contact_title">お名前（カナ）<span class="red">（必須）</span></span>
+				<div class="box table">
+					<span class="box1 cell">お名前（カナ）<span class="red">（必須）</span></span>
+				</div>
 			</li>
 			<li>
 				<span class="contact_area"><input type="text" name="name" value size="40" style="font-size: 25px"></span>
 			</li>
 
 			<li>
-				<span class="contact_title">お電話番号<span class="red">（必須）</span></span>
+				<div class="box table">
+					<span class="box1 cell">お電話番号<span class="red">（必須）</span></span>
+				</div>
 			</li>
 			<li>
-				<span class="contact_area"><input type="text" name="telno" value size="40" style="font-size: 25px"></span>
+				<span class="contact_area"><input type="text" name="telno" value size="40" style="font-size: 25px;"></span>
 			</li>
 
 			<li>
-				<span class="contact_title">郵便番号</span>
+				<div class="box table">
+					<span class="box1 cell">郵便番号</span>
+				</div>
 			</li>
 			<li>
 				<span class="contact_area"><input type="text" name="zipno" value size="40" style="font-size: 25px"></span>
 			</li>
 
 			<li>
-				<span class="contact_title">ご住所</span>
+				<div class="box table">
+					<span class="box1 cell">ご住所</span>
+				</div>
 			</li>
 			<li>
 				<span class="contact_area"><input type="text" name="address" value size="40" style="font-size: 25px"></span>
 			</li>
 				
 			<li>
-				<span class="contact_title">メールアドレス<span class="red">（必須）</span></span>
+				<div class="box table">
+					<span class="box1 cell">メールアドレス<span class="red">（必須）</span></span>
 			</li>
 			<li>
 				<span class="contact_area"><input type="text" name="mailadd" value size="40" style="font-size: 25px"></span>
 			</li>
 
 			<li>
-				<span class="contact_title">緊急またはお急ぎの場合はチェックを入れてください。</span>
+				<div class="box table">
+					<span class="box1 cell">緊急またはお急ぎの場合はチェックを入れてください。</span>
+				</div>
 			</li>
 			<li>
 				<span class="contact_area"><input type="checkbox" name="kinkyu" value="今すぐ解決してほしい">今すぐ解決してほしい</span>
 			</li>
 
 			<li>
-				<span class="contact_title">メッセージ本文<span class="red">（必須）</span></span>
+				<div class="box table">
+					<span class="box1 cell">メッセージ本文<span class="red">（必須）</span></span>
+				</div>
 			</li>
 			<li>
 				<span class="contact_area"><textarea name="message" cols="40" rows="10" style="font-size: 25px"></textarea></span>
 			</li>
 
 			<li>
-				<span class="contact_title"><input type="checkbox" name="accept" value="1"></span>
+				<div class="box table">
+					<span class="box1 cell"><input type="checkbox" name="accept" value="1"></span>
+				</div>
 			</li>
 			<li>
-				<span class="contact_title">内容をご確認の上チェックを入れてください<span class="red">（必須）</span></span>
+				<div class="box table">
+					<span class="box1 cell">内容をご確認の上チェックを入れてください<span class="red">（必須）</span></span>
+				</div>
 			</li>
 		</ul>
 		<div>
