c579edb9d24d5c2f55dab1a8b36441228afaae84
moco0524 <ogawa340910@gmail.com>
2022-06-18 10:54:10 +0900

    20220618 トップ画面修正１

--- resources/views/pages/top.blade.php
+++ resources/views/pages/top.blade.php
@@ -3,26 +3,35 @@
 @section('title', '足立区　便利屋クリーン　トップ')
 
 @section('content')
-<div class="center>
-	<div>
-		<div data-testid="linkElement" class="xQ_iF">
-			<img src="/images/lisence.png" alt="実績" style="width: ">
-		</div>
+<div class="center">
+	<div class="resizeimage">
+		<img src="/images/lisence.png">
 	</div>
-	
-	
-	<div>
+	<div class="resizeimage">
 		<img src="/images/koe-bnr.png">
 	</div>
 	<div>
 		<p class="font1">期間限定10％還元キャンペーン</p>
 		<p class="font1">「ホームページを見た」と言うだけで料金を割引！！</p>
 	</div>
-	<div>
+	<div class="resizeimage">
 		<img src="/images/price-service.png">
 		<p class="font1">ご相談から作業完了までの流れ</p>
-		<p class="font1">まずは　☎️ <a href="tel:08043466004">０１２０ー０６５ー９５５</a>　までお電話ください</p>
+		<p id="callphone" class="font1"></p>
 		<img src="/images/step.png">
 	</div>
 </div>
+<script type="text/javascript">
+	$(function(){
+		var ua = navigator.userAgent;
+		var phone = document.getElementById('callphone')
+		if (ua.indexOf("iPhone") > 0 || ua.indexOf("Android") > 0 && ua.indexOf("Mobile") > 0) {
+			phone.innerHTML = 'まずは　☎️ <a href="tel:08043466004">０１２０ー０６５ー９５５</a>　までお電話ください'
+		}else if (ua.indexOf("iPad") > 0 || ua.indexOf("Android") > 0) {
+			phone.innerHTML = 'まずは　☎️ <a href="tel:08043466004">０１２０ー０６５ー９５５</a>　までお電話ください'
+		} else {
+			phone.innerHTML = 'まずは　☎️ ０１２０ー０６５ー９５５　までお電話ください'
+		}
+	})
+</script>
 @endsection
