5e9010936f82a91fe1a19c8d2d8d0405a00141cf
moco0524 <ogawa340910@gmail.com>
2022-06-19 15:48:00 +0900

    20220619 ヴァリデーションチェック

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -26,6 +26,7 @@
 
         <!-- JQuery -->
         <script src="/js/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 
         <title>@yield('title')</title>
 
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -16,14 +16,14 @@
 	@csrf
 		<div class="item contact">
 			<label>
-				<input size="20" type="text" name="name" required placeholder="カナで入力して下さい" style="font-size: 20px;" />
+				<input size="20" type="text" name="name" id="name" required placeholder="カナで入力して下さい" style="font-size: 20px;" />
 				<span class="contacttitle size1">お名前（カナ）<span class="notice">必須</span></span>
 			</label>
 		</div>
 		<div class="item contact">
 			<label>
-				<input size="20" type="tel" name="telno" required style="font-size: 20px;" />
-				<span class="contacttitle size1">お電話番号<span class="notice">必須</span></span>
+				<input size="20" type="tel" name="telno" id="telno" required style="font-size: 20px;" />
+				<span class="contacttitle size1">お電話番号(半角数字のみ)<span class="notice">必須</span></span>
 			</label>
 		</div>
 		<div class="item contact">
@@ -40,7 +40,7 @@
 		</div>
 		<div class="item contact">
 			<label>
-				<input size="20" type="email" name="mail" required style="font-size: 20px;" />
+				<input size="20" type="email" name="mail" id="mail" required style="font-size: 20px;" />
 				<span class="contacttitle size1">メールアドレス<span class="notice">必須</span></span>
 			</label>
 		</div>
@@ -48,7 +48,7 @@
 			<label>
 				<span class="contacttitle size1">メール本文<span class="notice">必須</span></span>
 			</label>
-			<textarea placeholder="ご依頼内容またはご質問を入力してください" name="message" rows="5" style="font-size: 20px; display: flex;" required ></textarea>
+			<textarea placeholder="ご依頼内容またはご質問を入力してください" name="message" id="message" rows="5" style="font-size: 20px; display: flex;" required ></textarea>
 		</div>
 		<div class="item3 contact" style="align: center;">
 			<label class="size2">緊急またはお急ぎの場合はチェックを入れてください。</label>
@@ -84,6 +84,50 @@
 		} else {
 			$("#mail_send").hide();
 		}
+	})
+	
+	// ヴァリデーションチェック
+	$('#mail_send').click( function() {
+		if ($("#name").val() == "") {
+			alert('お名前を入力して下さい');
+			$("#name").css("background-color","#ff97c2");
+			return false;
+		} else if ($("#name").val().match(/^[ァ-ンヴーｧ-ﾝﾞﾟ\-]*$/)) {
+			$("#name").css("background-color","#ffffff");
+		} else {
+			alert('お名前をカナで入力して下さい');
+			$("#name").css("background-color","#ff97c2");
+			return false;
+		}
+		if ($("#telno").val() == "") {
+			alert('お電話番号を入力して下さい');
+			$("#telno").css("background-color","#ff97c2");
+		} else if ($("#telno").val().match(/^0\d{9,10}$/)) {
+			$("#telno").css("background-color","#ffffff");
+		} else {
+			alert('10桁または11桁の数字で入力して下さい');
+			$("#telno").css("background-color","#ff97c2");
+			return false;
+		}
+		if ($("#mail").val() == "") {
+			alert('メールアドレスを入力して下さい');
+			$("#mail").css("background-color","#ff97c2");
+			return false;
+		} else if ($("#mail").val().match(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/)) {
+			$("#mail").css("background-color","#ffffff");
+		} else {
+			alert('メールアドレスの形式となっていません');
+			$("#mail").css("background-color","#ff97c2");
+			return false;
+		}
+		if ($("#message").val() == "") {
+			alert('メール本文を入力して下さい');
+			$("#message").css("background-color","#ff97c2");
+			return false;
+		} else {
+			$("#message").css("background-color","#ffffff");
+		}
+		$('#mailsend').submit();
 	})
 </script>
 @endsection
